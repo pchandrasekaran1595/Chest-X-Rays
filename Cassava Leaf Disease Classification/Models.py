fe426ea014291b87f0d36fa09041fa37727e4cfe
Prashanth <67106624+pchandrasekaran1595@users.noreply.github.com>
2021-01-09 10:24:08 +0530

    Update Models.py

--- Cassava Leaf Disease Classification/Models.py
+++ Cassava Leaf Disease Classification/Models.py
@@ -3,7 +3,75 @@ from torch import nn, optim
 from torch.nn.utils import weight_norm as WN
 import torch.nn.functional as F
 
-class CNN(nn.Module):
+class CNN_7(nn.Module):
+    def __init__(this, in_channels=1, filter_sizes=None, HL=None, OL=None, AP_size=None, use_DP=True, DP=0.5):
+
+        super(CNN, this).__init__()
+
+        this.use_DP = use_DP
+
+        this.MP_ = nn.MaxPool2d(kernel_size=2)
+        this.AP_ = nn.AdaptiveAvgPool2d(output_size=AP_size)
+        this.DP_ = nn.Dropout(p=DP)
+
+        this.CN1 = nn.Conv2d(in_channels=in_channels, out_channels=filter_sizes[0], kernel_size=3, stride=1, padding=1)
+        this.BN1 = nn.BatchNorm2d(num_features=filter_sizes[0], eps=1e-5)
+
+        this.CN2 = nn.Conv2d(in_channels=filter_sizes[0], out_channels=filter_sizes[1], kernel_size=3, stride=1, padding=1)
+        this.BN2 = nn.BatchNorm2d(num_features=filter_sizes[1], eps=1e-5)
+
+        this.CN3 = nn.Conv2d(in_channels=filter_sizes[1], out_channels=filter_sizes[2], kernel_size=3, stride=1, padding=1)
+        this.BN3 = nn.BatchNorm2d(num_features=filter_sizes[2], eps=1e-5)
+
+        this.CN4 = nn.Conv2d(in_channels=filter_sizes[2], out_channels=filter_sizes[3], kernel_size=3, stride=1, padding=1)
+        this.BN4 = nn.BatchNorm2d(num_features=filter_sizes[3], eps=1e-5)
+
+        this.FC1 = nn.Linear(in_features=filter_sizes[3] * AP_size * AP_size, out_features=HL[0])
+        this.FC2 = nn.Linear(in_features=HL[0], out_features=HL[1])
+        this.FC3 = nn.Linear(in_features=HL[1], out_features=OL)
+
+    def getOptimizer(this, lr=1e-3, wd=0):
+        return optim.Adam(this.parameters(), lr=lr, weight_decay=wd)
+
+    def getMultiStepLR(this, optimizer=None, milestones=None, gamma=0.1):
+        return optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=milestones, gamm=gamma)
+    
+    def getPlateauLR(this, optimizer=None, patience=5, eps=1e-8):
+        return optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer, patience=patience, eps=eps, verbose=True)
+
+    def forward(this, x):
+        if this.use_DP:
+            x = F.relu(this.MP_(this.BN1(this.CN1(x))))
+            x = F.relu(this.MP_(this.BN2(this.CN2(x))))
+            x = F.relu(this.MP_(this.BN3(this.CN3(x))))
+            x = F.relu(this.MP_(this.BN4(this.CN4(x))))
+            x = F.relu(this.MP_(this.BN5(this.CN5(x))))
+
+            x = this.AP_(x)
+            x = x.view(x.shape[0], -1)
+
+            x = F.relu(this.DP_(this.FC1(x)))
+            x = F.relu(this.DP_(this.FC2(x)))
+            x = F.log_softmax(this.FC3(x), dim=1)
+
+            return x
+        else:
+            x = F.relu(this.MP_(this.BN1(this.CN1(x))))
+            x = F.relu(this.MP_(this.BN2(this.CN2(x))))
+            x = F.relu(this.MP_(this.BN3(this.CN3(x))))
+            x = F.relu(this.MP_(this.BN4(this.CN4(x))))
+            x = F.relu(this.MP_(this.BN5(this.CN5(x))))
+
+            x = this.AP_(x)
+            x = x.view(x.shape[0], -1)
+
+            x = F.relu(this.FC1(x))
+            x = F.relu(this.FC2(x))
+            x = F.log_softmax(this.FC3(x), dim=1)
+
+            return x
+
+class CNN_11(nn.Module):
     def __init__(this, in_channels=1, filter_sizes=None, HL=None, OL=None, AP_size=3, use_DP=True, DP=0.5):
         super(CNN, this).__init__()
 
